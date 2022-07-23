a2fa7f3e761967c1d23170659d1d6a3b7c2e0033
yamazaki sota <kizumini10@gmail.com>
2022-07-23 10:56:37 +0900

    FileWrite

--- src/jp/alhinc/yamazaki_sota/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/yamazaki_sota/calculate_sales/CalculateSales.java
@@ -1,9 +1,12 @@
 package jp.alhinc.yamazaki_sota.calculate_sales;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 
 public class CalculateSales {
@@ -39,21 +42,92 @@ public class CalculateSales {
 	    	System.out.println(filenames[i]);
 	    }
 	    
+	    System.out.println(filenames[0]);
+	    int sapporo = 0;
+	    int sendai = 0;
+	    int tokyo = 0;
+	    int nagoya = 0;
+	    int osaka = 0;
 	    
-//	    for(int i = 0; i<filenames.length; i++) {
-//	    	if(filenames[i].matches(".+rcd$")) {
-//	    		try {
-//	    			File file = new File(args[0], "filenames[i]");
-//	    			FileReader fr = new FileReader(file);
-//	    			BufferedReader br = new BufferedReader(fr);
-//	    			String p;
-//	    			while((p = br.readLine()) != null){
-//	    				System.out.println(p);
-//	    			}
-//	    		}catch(IOException e) {
-//	    			System.out.println(e);
-//	    		}
-//	    	}
-//	    }
+	    
+	    
+	    for(int i = 0; i<filenames.length; i++) {
+	    	if(filenames[i].matches(".+rcd$")) {
+	    		try {
+	    			File file = new File(args[0], filenames[i]);
+	    			FileReader fr = new FileReader(file);
+	    			BufferedReader br = new BufferedReader(fr);
+	    			String key;
+	    			key = br.readLine();
+	    			System.out.println(key);
+	    			int value;
+	    			value = Integer.parseInt(br.readLine());
+	    			System.out.println(value);
+	    			
+	    			if(key.equals("001")) {
+	    				sapporo += value;
+	    				System.out.println(sapporo);
+	    				shopsdeta.put(key,sapporo);
+	    			}else if(key.equals("002")) {
+	    				sendai += value;
+	    				System.out.println(sendai);
+	    				shopsdeta.put(key,sendai);
+	    			}else if(key.equals("003")) {
+	    				tokyo += value;
+	    				System.out.println(tokyo);
+	    				shopsdeta.put(key,tokyo);
+	    			}else if(key.equals("004")) {
+	    				nagoya += value;
+	    				System.out.println(nagoya);
+	    				shopsdeta.put(key,nagoya);
+	    			}else if(key.equals("005")) {
+	    				osaka += value;
+	    				System.out.println(osaka);
+	    				shopsdeta.put(key,osaka);
+	    			}
+	    		}catch(IOException e) {
+	    			System.out.println(e);
+	    		}
+	    	}
+	    }
+	    
+	    System.out.println(shopsdeta.get("001"));
+		System.out.println(shopsdeta.get("002"));
+		System.out.println(shopsdeta.get("003"));
+		System.out.println(shopsdeta.get("004"));
+		System.out.println(shopsdeta.get("005"));
+	    
+		
+		try {
+			File file = new File("/Users/yamazaki/training/CalculateSales/shopdetas/branch.out");
+			if(file.createNewFile()) {
+				System.out.println("ファイルが作成されました");
+			}else {
+				System.out.println("ファイルの作成に失敗しました");
+			}
+			FileWriter fw = new FileWriter(file,true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			PrintWriter pw = new PrintWriter(bw);
+			pw.println("001,札幌支店," + shopsdeta.get("001"));
+			pw.println("002,仙台支店," + shopsdeta.get("002"));
+			pw.println("003,東京支店," + shopsdeta.get("003"));
+			pw.println("004,名古屋支店," + shopsdeta.get("004"));
+			pw.println("005,大阪支店," + shopsdeta.get("005"));
+			pw.close();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			File file = new File(args[0], "branch.out");
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+			String s ;
+			while((s = br.readLine()) != null) {
+				System.out.println(s);
+			}
+		}catch(IOException e) {
+			System.out.println(e);
+		}
 	}
 }
